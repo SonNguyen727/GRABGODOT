93b5d0fb57da9153d61621bff96b88cdec2f1a5b
Nguyễn Ngọc Tùng Sơn <24070343@vnu.edu.vn>
2026-05-18 00:11:53 +0700

    test

--- robot.gd
+++ robot.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
-var speed = 200
-
+var speed = 50
 var grabbed = false
 var arm = null
 
 @onready var anim = $AnimatedSprite2D
 
-func _process(delta):
+
+func _physics_process(delta):
 
 	if grabbed and arm != null:
 
 		anim.play("idle")
 
-		global_position = global_position.lerp(
-			arm.global_position,
-			15 * delta
-		)
 
+		var target_position = arm.global_position
+		velocity = (target_position - global_position) * 15
+		
+
+		move_and_slide()
 		return
+
 
 	anim.play("moving")
 
-	position.x += speed * delta
+
+	velocity.x = speed
+	velocity.y = 0
+	
+
+	move_and_slide()
