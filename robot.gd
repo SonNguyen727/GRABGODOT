089dc588b02860aea9ff1f2cbbddb60f922c62f2
taijinn <24070763@vnu.edu.vn>
2026-05-17 21:10:52 +0700

    2nd commit

--- robot.gd
+++ robot.gd
@@ -5,12 +5,21 @@ var speed = 200
 var grabbed = false
 var arm = null
 
+@onready var anim = $AnimatedSprite2D
+
 func _process(delta):
+
 	if grabbed and arm != null:
+
+		anim.play("idle")
+
 		global_position = global_position.lerp(
 			arm.global_position,
 			15 * delta
 		)
+
 		return
+
+	anim.play("moving")
 
 	position.x += speed * delta
