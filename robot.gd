71d2e5a10eb528f1c36b694134d1beddf679908c
ngocnu-12 <1208ngocnu@gmail.com>
2026-05-18 00:34:15 +0700

    art

--- robot.gd
+++ robot.gd
@@ -1,16 +1,32 @@
 extends CharacterBody2D
 
-var speed = 200
-
+var speed = 50
 var grabbed = false
 var arm = null
 
-func _process(delta):
+@onready var anim = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+
 	if grabbed and arm != null:
-		global_position = global_position.lerp(
-			arm.global_position,
-			15 * delta
-		)
+
+		anim.play("idle")
+
+
+		var target_position = arm.global_position
+		velocity = (target_position - global_position) * 15
+		
+
+		move_and_slide()
 		return
 
-	position.x += speed * delta
+
+	anim.play("moving")
+
+
+	velocity.x = speed
+	velocity.y = 0
+	
+
+	move_and_slide()
